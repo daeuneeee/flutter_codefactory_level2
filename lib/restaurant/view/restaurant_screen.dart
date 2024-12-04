2dd14ab802cf64ed9052a44932c2a000a5979566
daeuneeee <ida-eun@ida-euns-iMac.local>
2024-12-04 10:47:14 +0900

    data modeling

---
+++ lib/restaurant/view/restaurant_screen.dart
@@ -0,0 +1,68 @@
+import 'dart:math';
+
+import 'package:actual/common/const/data.dart';
+import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  const RestaurantScreen({super.key});
+
+  Future<List> paginateRestaurant() async {
+    final dio = Dio();
+
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      'http://$ip/restaurant',
+      options: Options(
+        headers: {
+          'authorization': 'Bearer $accessToken',
+        },
+      ),
+    );
+
+    return resp.data['data'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: FutureBuilder<List>(
+            future: paginateRestaurant(),
+            builder: (context, AsyncSnapshot<List> snapshot) {
+              if (!snapshot.hasData) {
+                return Container();
+              }
+
+              return ListView.separated(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) {
+                  final item = snapshot.data![index];
+
+                  // parsed item
+                  final pItem = RestaurantModel.fromJson(
+                    json: item,
+                  );
+
+                  return RestaurantCard.fromModel(
+                    model: pItem,
+                  );
+                },
+                separatorBuilder: (_, index) {
+                  return SizedBox(
+                    height: 16,
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
