c17663086ca81c6290f84787bbd128fa5cb62f8b
daeuneeee <ida-eun@ida-euns-iMac.local>
2024-12-05 14:42:37 +0900

    jsonSerialzable, retrofit, dio interceptor

---
+++ lib/common/dio/dio.dart
@@ -0,0 +1,118 @@
+import 'package:actual/common/const/data.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class CustomInterceptor extends Interceptor {
+  final FlutterSecureStorage storage;
+
+  CustomInterceptor({
+    required this.storage,
+  });
+
+  // 1) 요청 보낼 때
+  // 요청이 보내질 때 마다 요청의 header에 accessToken: ture라는 값이 있다면
+  // 실제 토큰을 가져와서 (storage) authorization: bearer $token으로 헤더를 변경한다.
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    // print(
+    //   '[REQ][${options.method} ${options.uri}]',
+    // );
+    if (options.headers['accessToken'] == 'true') {
+      // 헤더 삭제
+      options.headers.remove('accessToken');
+
+      final token = await storage.read(key: ACCESS_TOKEN_KEY);
+
+      // 실제 토큰으로 대체
+      options.headers.addAll({'authorization': 'Bearer $token'});
+    }
+
+    if (options.headers['refreshToken'] == 'true') {
+      // 헤더 삭제
+      options.headers.remove('refreshToken');
+
+      final token = await storage.read(key: REFRESH_TOKEN_KEY);
+
+      // 실제 토큰으로 대체
+      options.headers.addAll({'authorization': 'Bearer $token'});
+    }
+
+    return super.onRequest(options, handler);
+  }
+
+  // 2) 응답을 받을 때
+  @override
+  void onResponse(
+    Response response,
+    ResponseInterceptorHandler handler,
+  ) {
+    // print(
+    //   '[RES][${response.requestOptions.method} ${response.requestOptions.uri}]',
+    // );
+
+    super.onResponse(response, handler);
+  }
+
+  // 3) 에러가 났을 때
+  @override
+  void onError(
+    DioError err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    // 401 에러가 발생했을 때 (status code)
+    // 토큰을 재발급 받는 시도를 하고 토큰이 재발급되면 새로운 토큰으로 다시 요청한다.
+    // print(
+    //   '[ERR][${err.requestOptions.method} ${err.requestOptions.uri}]',
+    // );
+
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+
+    // refreshToken이 아예 없으면 에러를 던진다.
+    if (refreshToken == null) {
+      // 에러를 던질 때에는 handler.reject를 사용한다.
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    if (isStatus401 && !isPathRefresh) {
+      final dio = Dio();
+
+      try {
+        final resp = await dio.post(
+          'http://$ip/auth/token',
+          options: Options(
+            headers: {
+              'authorization': 'Bearer $refreshToken',
+            },
+          ),
+        );
+
+        final accessToken = resp.data['accessToken'];
+
+        final options = err.requestOptions;
+
+        // 토큰 변경
+        options.headers.addAll({
+          'authorization': 'Bearer $accessToken',
+        });
+
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+        // 요청 재전송
+        final response = await dio.fetch(options);
+        // 응답값을 넣어주면 실제로 요청을 실행한 화면에서는 에러가 나지 않은 것처럼 인식 가능
+        return handler.resolve(response);
+      } on DioError catch (e) {
+        // 에러 반환
+        return handler.reject(e);
+      }
+    }
+
+    return handler.reject(err);
+  }
+}
--- lib/restaurant/model/restaurant_detail_model.dart
+++ lib/restaurant/model/restaurant_detail_model.dart
@@ -1,16 +1,11 @@
 import 'package:actual/common/const/data.dart';
+import 'package:actual/common/utils/data_utils.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-// "detail": "!!!리뷰 EVENT & 비조리 EVENT 진행중!!!\n    \n@ 기본적으로 매콤합니다 @\n@ 덜맵게 가능하니 요청사항에 적어주세요 @\n@ 1인분 배달 가능합니다 @",
-// "products": [
-// {
-// "id": "77491ba3-6351-4635-8b30-c891bf6574ab",
-// "name": "떡볶이",
-// "detail": "전통 떡볶이의 정석! 원하는대로 맵기를 선택하고 추억의 떡볶이맛에 빠져보세요! 쫀득한 쌀떡과 말랑한 오뎅의 완벽한 조화! 잘익은 반숙 계란은 덤!",
-// "imgUrl": "/img/떡볶이/떡볶이.jpg",
-// "price": 10000
-// },
+part 'restaurant_detail_model.g.dart';
 
+@JsonSerializable()
 class RestaurantDetailModel extends RestaurantModel {
   final String detail;
   final List<RestaurantProductModel> products;
@@ -29,36 +24,41 @@ class RestaurantDetailModel extends RestaurantModel {
     required this.products,
   });
 
-  factory RestaurantDetailModel.fromJson({
-    required Map<String, dynamic> json,
-  }) {
-    return RestaurantDetailModel(
-      id: json['id'],
-      name: json['name'],
-      thumbUrl: 'http://$ip${json['thumbUrl']}',
-      tags: List<String>.from(json['tags']),
-      priceRange: RestaurantPriceRange.values
-          .firstWhere((e) => e.name == json['priceRange']),
-      ratings: json['ratings'],
-      ratingsCount: json['ratingsCount'],
-      deliveryTime: json['deliveryTime'],
-      deliveryFee: json['deliveryFee'],
-      detail: json['detail'],
-      products: json['products']
-          .map<RestaurantProductModel>(
-            (x) => RestaurantProductModel.fromJson(
-              json: x,
-            ),
-          )
-          .toList(),
-    );
-  }
+  factory RestaurantDetailModel.fromJson(Map<String, dynamic> json) =>
+      _$RestaurantDetailModelFromJson(json);
+
+  // factory RestaurantDetailModel.fromJson({
+  //   required Map<String, dynamic> json,
+  // }) {
+  //   return RestaurantDetailModel(
+  //     id: json['id'],
+  //     name: json['name'],
+  //     thumbUrl: 'http://$ip${json['thumbUrl']}',
+  //     tags: List<String>.from(json['tags']),
+  //     priceRange: RestaurantPriceRange.values
+  //         .firstWhere((e) => e.name == json['priceRange']),
+  //     ratings: json['ratings'],
+  //     ratingsCount: json['ratingsCount'],
+  //     deliveryTime: json['deliveryTime'],
+  //     deliveryFee: json['deliveryFee'],
+  //     detail: json['detail'],
+  //     products: json['products']
+  //         .map<RestaurantProductModel>(
+  //           (x) => RestaurantProductModel.fromJson(
+  //             json: x,
+  //           ),
+  //         )
+  //         .toList(),
+  //   );
+  // }
 }
 
+@JsonSerializable()
 class RestaurantProductModel {
   final String id;
   final String name;
   final String detail;
+  @JsonKey(fromJson: DataUtils.pathToUrl)
   final String imgUrl;
   final int price;
 
@@ -70,15 +70,18 @@ class RestaurantProductModel {
     required this.price,
   });
 
-  factory RestaurantProductModel.fromJson({
-    required Map<String, dynamic> json,
-  }) {
-    return RestaurantProductModel(
-      id: json['id'],
-      name: json['name'],
-      detail: json['detail'],
-      imgUrl: 'http://$ip${json['imgUrl']}',
-      price: json['price'],
-    );
-  }
+  factory RestaurantProductModel.fromJson(Map<String, dynamic> json) =>
+      _$RestaurantProductModelFromJson(json);
+
+  // factory RestaurantProductModel.fromJson({
+  //   required Map<String, dynamic> json,
+  // }) {
+  //   return RestaurantProductModel(
+  //     id: json['id'],
+  //     name: json['name'],
+  //     detail: json['detail'],
+  //     imgUrl: 'http://$ip${json['imgUrl']}',
+  //     price: json['price'],
+  //   );
+  // }
 }
